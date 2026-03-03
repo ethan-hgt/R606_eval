8fae76e7064454f85c51fad396b2e465ee91a9e7
ethan <ethan.hugerot@hotmail.fr>
2026-03-03 09:23:50 +0100

    refactor index.php  séparation vue

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><title>R6.06 Maintenance applicative</title></head>
-<body>
-<header>
-    <h1>R6.06 Maintenance applicative</h1><h2 style="color: crimson">Evaluation</h2>
-    <p style="color: crimson">Modifiez ce projet à l'aide des outils vus ensemble pour améliorer la maintenabilité de ce projet et déployez le sur le serveur mis à votre disposition</p><p style="color: crimson">Vous êtes libre de modifier ce que vous souhaitez sur le projet, chaque amélioration (ou début d'amélioration) sera prise en compte dans la notation</p>
-    <p style="color: crimson; font-weight: bold; border: solid 2px crimson; padding: 5px; width: fit-content;">Pensez à inviter cdiiv sur votre projet Github</p>
-</header>
-
 <?php
 $config = require __DIR__ . '/config.php';
 require_once __DIR__ . '/src/ConnexionBdd.php';
@@ -23,16 +15,6 @@ require_once __DIR__ . '/migrate.php';
 runMigrations($p);
 
 $tableTexte = new TableTexte($p);
-$d = $tableTexte->recupererTout();
-?>
+$rows = $tableTexte->recupererTout();
 
-<table>
-    <thead style="font-weight: bold;"><tr><td style="border: solid black 1px">Id</td><td style="border: solid black 1px">Text</td></tr></thead>
-    <tbody>
-        <?php foreach ($d as $row) { ?>
-            <tr><td style="border: solid black 1px"><?= $row['id'] ?></td><td style="border: solid black 1px"><?= $row['text'] ?></td></tr>
-        <?php } ?>
-    </tbody>
-</table>
-</body>
-</html>
+require __DIR__ . '/views/index.view.php';
